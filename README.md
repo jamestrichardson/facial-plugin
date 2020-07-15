a7c4c8a1a77de37e3a4c38d44b50960ad0bb85cc
James Richardson <teknofile@teknofile.org>
2020-07-15 09:48:48 -0600

    fixing some typos, added docs

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # facial-plugin
-A PiWiGo plugin to do some facial recognition 
+
+A PiWiGo plugin to do some facial recognition
 
 * Internal name: `facial-plugin` (directory name in `plugins/`)
 * plugin page: TBD
 
 ## Development Status
+
 This is initially a proof of concept. That I hope to one day have functioning.
+
+## Introduction
+
+This plugin is to facilitate identifing which pictures do have faces within
+them, and then whose face that belongs to. So, more plainly:
+
+1. On the administration of `albums` page we want an option to "Scan for faces"
+2. Perform the scan on all of the photos within that album
+3. Identify each face (or place a placeholder of "Unknown person #3089" to it)
+
+To that effect, we will write code for:
+
+1. An admin panel, where the admin can admin the adminy stuff?
+2. An extension to the albums page so that we can scan albums (we might instead use batch manager to select photos that way?)
+3. Another event hook that will display the people identified in the photo
--- admin.php
+++ admin.php
@@ -1,37 +1,38 @@
 <?php
 /**
  * This is the main administration page, if you have only one admin page you can put
- * directly its code here or useing the tabsheet system link below
+ * directly its code here or using the tabsheet system link below
  */
 
- defined('FACIAL_PATH') or die('Hacking attempt!');
+// Chech whether we are indeed included by Piwigo.
+defined('FACIAL_PATH') or die('Hacking attempt!');
 
- global $template, $page, $conf;
+global $template, $page, $conf;
 
- // get current tab
- $page['tab'] = isset($_GET['tab']) ? $_GET['tab'] : $page['tab'] = 'home';
+// get current tab
+$page['tab'] = isset($_GET['tab']) ? $_GET['tab'] : $page['tab'] = 'home';
 
- // plugin tabsheet is not present on photo page
- if($page['tabh'] != 'photo') {
-   // tabsheet
-   include_once(PHPWG_ROOT_PATH . 'admin/include/tabsheet.class.php');
-   $tabsheet = new tabsheet();
-   $tabsheet->set_id('facial');
+// plugin tabsheet is not present on photo page
+if($page['tab'] != 'photo') {
+  // tabsheet
+  include_once(PHPWG_ROOT_PATH . 'admin/include/tabsheet.class.php');
+  $tabsheet = new tabsheet();
+  $tabsheet->set_id('facial');
 
-   $tabsheet->add('home', l10n('Welcome'), FACIAL_ADMIN . '-home');
-   $tabsheet->add('config', l10n('Configuration'), FACIAL_ADMIN . '-config');
-   $tabsheet->select($page['tab']);
-   $tabsheet->assign();
- }
+  $tabsheet->add('home', l10n('Welcome'), FACIAL_ADMIN . '-home');
+  $tabsheet->add('config', l10n('Configuration'), FACIAL_ADMIN . '-config');
+  $tabsheet->select($page['tab']);
+  $tabsheet->assign();
+}
 
- // include page
- include(FACIAL_PATH . 'admin/' . $page['tab'] . '.php');
+// include page
+include(FACIAL_PATH . 'admin/' . $page['tab'] . '.php');
 
- //template vars
- $template->assign(array(
-   'FACIAL_PATH' => FACIAL_PATH, // used for images, scripts, ... access
-   'FACIAL_ABS_PATH' => realpath(FACIAL_PATH), // used for template inclusion (Smarty needs real path)
-   'FACIAL_ADMIN' => FACIAL_ADMIN));
+//template vars
+$template->assign(array(
+  'FACIAL_PATH' => FACIAL_PATH, // used for images, scripts, ... access
+  'FACIAL_ABS_PATH' => realpath(FACIAL_PATH), // used for template inclusion (Smarty needs real path)
+  'FACIAL_ADMIN' => FACIAL_ADMIN));
 
 // send page content
 $template->assign_var_from_handler('ADMIN_CONTENT', 'facial_content');
