7968f167be24c1b5ccacd030d694239a18f03ccd
James Richardson <james@richardsons.us>
2025-10-22 03:13:25 +0000

    Fix PHPCS 'Hacking attempt!' error
    - Removed autoload of functions.inc.php from composer.json (was causing FACIAL_PATH constant check to fail)
    - Added XDEBUG_MODE=coverage to test coverage scripts
    - Added PHPCS configuration file with PSR12 standard
    - Updated run-tests.sh to enable Xdebug coverage mode
    
    This fixes the GitHub Actions code-quality job failure and enables proper code coverage reporting for SonarQube.

---
+++ .phpcs-bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * PHPCS Bootstrap file
+ * Defines constants needed to prevent "Hacking attempt!" errors
+ * 
+ * This file is loaded before PHPCS scans the codebase
+ */
+
+// Define constants that are normally set by Piwigo
+if (!defined('FACIAL_PATH')) {
+    define('FACIAL_PATH', __DIR__ . '/');
+}
+
+if (!defined('PHPWG_ROOT_PATH')) {
+    define('PHPWG_ROOT_PATH', dirname(__DIR__, 3) . '/');
+}
+
+// Define other common Piwigo constants
+if (!defined('IMAGES_TABLE')) {
+    define('IMAGES_TABLE', 'piwigo_images');
+}
+
+if (!defined('TAGS_TABLE')) {
+    define('TAGS_TABLE', 'piwigo_tags');
+}
+
+if (!defined('IMAGE_TAG_TABLE')) {
+    define('IMAGE_TAG_TABLE', 'piwigo_image_tag');
+}
+
+// Mock global variables
+global $conf, $logger, $prefixeTable;
+$conf = [];
+$prefixeTable = 'piwigo_';
